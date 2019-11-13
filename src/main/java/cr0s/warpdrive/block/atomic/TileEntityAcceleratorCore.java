c37e213c1dac8b8ae92af50d6a383289aef6e7ba
Unknown <lemadec.fr@gmail.com>
2019-11-13 01:30:02 +0100

    Improved particle injector visual clues when unable to inject
    - mark the injector location instead of the core
    - report injector with no valid items in stock

--- src/main/java/cr0s/warpdrive/block/atomic/TileEntityAcceleratorCore.java
+++ src/main/java/cr0s/warpdrive/block/atomic/TileEntityAcceleratorCore.java
@@ -406,7 +406,7 @@ public class TileEntityAcceleratorCore extends TileEntityAbstractEnergyCoreOrCon
 		final Collection<Object> inventories = InventoryWrapper.getConnectedInventories(tileEntity.getWorld(), tileEntity.getPos());
 		if (inventories.isEmpty()) {
 			PacketHandler.sendSpawnParticlePacket(world, "jammed", (byte) 5,
-			                                      new Vector3(pos),
+			                                      new Vector3(vInjector.x, vInjector.y, vInjector.z),
 			                                      new Vector3(0.0D, 0.0D, 0.0D),
 			                                      1.0F, 1.0F, 1.0F,
 			                                      1.0F, 1.0F, 1.0F,
@@ -445,6 +445,12 @@ public class TileEntityAcceleratorCore extends TileEntityAbstractEnergyCoreOrCon
 			if (WarpDriveConfig.LOGGING_ACCELERATOR) {
 				WarpDrive.logger.debug(this + " No valid item found to inject");
 			}
+			PacketHandler.sendSpawnParticlePacket(world, "jammed", (byte) 5,
+			                                      new Vector3(vInjector.x, vInjector.y, vInjector.z),
+			                                      new Vector3(0.0D, 0.0D, 0.0D),
+			                                      1.0F, 1.0F, 1.0F,
+			                                      1.0F, 1.0F, 1.0F,
+			                                      32);
 			return;
 		}
 		//noinspection ConstantConditions
