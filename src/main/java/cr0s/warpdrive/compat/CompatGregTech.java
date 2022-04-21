5a7bc4267e654fb13e4ba55e62b489668143a6d5
LemADEC <lemadec.fr@gmail.com>
2022-04-21 22:46:25 +0200

    Added GTCEu rotation compatibility

--- src/main/java/cr0s/warpdrive/compat/CompatGregTech.java
+++ src/main/java/cr0s/warpdrive/compat/CompatGregTech.java
@@ -5,14 +5,19 @@ import cr0s.warpdrive.api.ITransformation;
 import cr0s.warpdrive.api.WarpDriveText;
 import cr0s.warpdrive.config.WarpDriveConfig;
 
+import javax.annotation.Nonnull;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import net.minecraftforge.common.util.Constants.NBT;
 
 public class CompatGregTech implements IBlockTransformer {
 	
@@ -55,75 +60,123 @@ public class CompatGregTech implements IBlockTransformer {
 	}
 	
 	/*
-	As of gregtech-1.12.2-1.0.124
+	As of gregtech-1.12.2-2.1.4
 	gregtech.api.pipenet.tile.TileEntityPipeBase (cables and fluid pipes)
 	    BlockedConnections int (1 << side.func_176745_a())
+	    Connections int (1 << side.func_176745_a())
+		Covers[].Side byte 2 5 3 4
 	gregtech.api.metatileentity.MetaTileEntityHolder (machine)
 		MetaTileEntity.FrontFacing int 2 5 3 4
+		MetaTileEntity.OutputFacing int 2 5 3 4
+		MetaTileEntity.OutputFacingF int 2 5 3 4
+		MetaTileEntity.Covers[].Side byte 2 5 3 4
 	*/
 	
 	// -----------------------------------------    {  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 };
 	private static final int[]   rotFacing        = {  0,  1,  5,  4,  2,  3,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 };
 	
+	private void rotateConnection(@Nonnull final NBTTagCompound tagCompound, @Nonnull final String key, final byte rotationSteps) {
+		if (!tagCompound.hasKey(key)) {
+			return;
+		}
+		
+		final int connectionsOld = tagCompound.getInteger(key);
+		int connectionsNew = connectionsOld & 0x3; // keep bottom and top
+		for (final EnumFacing enumFacing : EnumFacing.HORIZONTALS) {
+			// get state for current face
+			final int indexFacingOld = enumFacing.getIndex();
+			final boolean isBlocked = (connectionsOld & (1 << indexFacingOld)) != 0;
+			if (!isBlocked) {
+				continue;
+			}
+			// get new face
+			final int indexFacingNew;
+			switch (rotationSteps) {
+			case 1:
+				indexFacingNew = rotFacing[indexFacingOld];
+				break;
+			case 2:
+				indexFacingNew = rotFacing[rotFacing[indexFacingOld]];
+				break;
+			case 3:
+				indexFacingNew = rotFacing[rotFacing[rotFacing[indexFacingOld]]];
+				break;
+			default:
+				indexFacingNew = indexFacingOld;
+				break;
+			}
+			// apply state on new face
+			connectionsNew |= (1 << indexFacingNew);
+		}
+		tagCompound.setInteger(key, connectionsNew);
+	}
+	
+	private void rotateFaceInteger(@Nonnull final NBTTagCompound tagCompound, @Nonnull final String key, final byte rotationSteps) {
+		if (!tagCompound.hasKey(key)) {
+			return;
+		}
+		
+		final int frontFacing = tagCompound.getInteger(key);
+		switch (rotationSteps) {
+		case 1:
+			tagCompound.setInteger(key, rotFacing[frontFacing]);
+			break;
+		case 2:
+			tagCompound.setInteger(key, rotFacing[rotFacing[frontFacing]]);
+			break;
+		case 3:
+			tagCompound.setInteger(key, rotFacing[rotFacing[rotFacing[frontFacing]]]);
+			break;
+		default:
+			break;
+		}
+	}
+	
+	private void rotateCovers(@Nonnull final NBTTagCompound tagCompound, final byte rotationSteps) {
+		final String key = "Covers";
+		if (!tagCompound.hasKey(key)) {
+			return;
+		}
+		
+		final NBTTagList tagListCovers = tagCompound.getTagList("Covers", NBT.TAG_COMPOUND);
+		final int count = tagListCovers.tagCount();
+		for (int index = 0; index < count; index++) {
+			final NBTTagCompound tagCover = (NBTTagCompound) tagListCovers.get(index);
+			final int side = tagCover.getByte("Side");
+			switch (rotationSteps) {
+			case 1:
+				tagCover.setByte("Side", (byte) rotFacing[side]);
+				break;
+			case 2:
+				tagCover.setByte("Side", (byte) rotFacing[rotFacing[side]]);
+				break;
+			case 3:
+				tagCover.setByte("Side", (byte) rotFacing[rotFacing[rotFacing[side]]]);
+				break;
+			}
+		}
+	}
+	
 	@Override
 	public int rotate(final Block block, final int metadata, final NBTTagCompound nbtTileEntity, final ITransformation transformation) {
 		final byte rotationSteps = transformation.getRotationSteps();
-		if (rotationSteps == 0 || nbtTileEntity == null) {
+		if ( rotationSteps == 0
+		  || nbtTileEntity == null ) {
 			return metadata;
 		}
 		
 		// BlockedConnections int (1 << side.func_176745_a())
-		if (nbtTileEntity.hasKey("BlockedConnections")) {// @TODO: to be tested once GTCE implements it for players
-			final int blockedConnectionsOld = nbtTileEntity.getInteger("BlockedConnections");
-			int blockedConnectionsNew = blockedConnectionsOld & 0x3; // keep bottom and top
-			for (final EnumFacing enumFacing : EnumFacing.HORIZONTALS) {
-				// get state for current face
-				final int indexFacingOld = enumFacing.getIndex();
-				final boolean isBlocked = (blockedConnectionsOld & (1 << indexFacingOld)) != 0;
-				if (!isBlocked) {
-					continue;
-				}
-				// get new face
-				final int indexFacingNew;
-				switch (rotationSteps) {
-				case 1:
-					indexFacingNew = rotFacing[indexFacingOld];
-					break;
-				case 2:
-					indexFacingNew = rotFacing[rotFacing[indexFacingOld]];
-					break;
-				case 3:
-					indexFacingNew = rotFacing[rotFacing[rotFacing[indexFacingOld]]];
-					break;
-				default:
-					indexFacingNew = indexFacingOld;
-					break;
-				}
-				// apply state on new face
-				blockedConnectionsNew |= (1 << indexFacingNew);
-			}
-			nbtTileEntity.setInteger("BlockedConnections", blockedConnectionsNew);
-		}
+		rotateConnection(nbtTileEntity, "BlockedConnections", rotationSteps);
+		rotateConnection(nbtTileEntity, "Connections", rotationSteps);
+		rotateCovers(nbtTileEntity, rotationSteps);
 		
 		// MetaTileEntity.FrontFacing int 2 5 3 4
 		if (nbtTileEntity.hasKey("MetaTileEntity")) {
 			final NBTTagCompound tagCompoundMetaTileEntity = nbtTileEntity.getCompoundTag("MetaTileEntity");
-			if (tagCompoundMetaTileEntity.hasKey("FrontFacing")) {
-				final int frontFacing = tagCompoundMetaTileEntity.getInteger("FrontFacing");
-				switch (rotationSteps) {
-				case 1:
-					tagCompoundMetaTileEntity.setInteger("FrontFacing", rotFacing[frontFacing]);
-					break;
-				case 2:
-					tagCompoundMetaTileEntity.setInteger("FrontFacing", rotFacing[rotFacing[frontFacing]]);
-					break;
-				case 3:
-					tagCompoundMetaTileEntity.setInteger("FrontFacing", rotFacing[rotFacing[rotFacing[frontFacing]]]);
-					break;
-				default:
-					break;
-				}
-			}
+			rotateFaceInteger(tagCompoundMetaTileEntity, "FrontFacing", rotationSteps);
+			rotateFaceInteger(tagCompoundMetaTileEntity, "OutputFacing", rotationSteps);
+			rotateFaceInteger(tagCompoundMetaTileEntity, "OutputFacingF", rotationSteps);
+			rotateCovers(tagCompoundMetaTileEntity, rotationSteps);
 		}
 		
 		return metadata;
